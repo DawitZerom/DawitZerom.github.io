a4843f560f67e28f3795d9bc6a2d252dd61874c9
DawitZerom <109552726+DawitZerom@users.noreply.github.com>
2022-07-29 01:03:53 -0500

    pdf

---
+++ untitled folder/main.js
@@ -0,0 +1,15 @@
+
+    
+    function kitty(){
+        document.getElementById('myImage')
+        .src="kitty.jpg";
+        document.getElementById('message')
+        .innerHTML="Furry Friend of the Day";
+    }
+      
+    function puppy(){
+        document.getElementById('myImage')
+        .src="puppy.jpg";
+        document.getElementById('message')
+        .innerHTML="Furry Friend of the Day";
+    }
