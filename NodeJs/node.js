5c3b6df82c168403db0a93cb747fbb68e731fe21
DawitZerom <109552726+DawitZerom@users.noreply.github.com>
2022-08-02 01:18:59 -0500

    npm and assighnment

---
+++ NodeJs/node.js
@@ -0,0 +1,17 @@
+var http = require("http");
+http
+  .createServer(function (req, res) {
+    res.write(" Hello World");
+    res.end();
+  })
+  .listen(8080);
+
+var http = require("http");
+var dt = require("./myModule");
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("The date and time are currently: " + dt.myDate());
+    res.end();
+  })
+  .listen(8080);
